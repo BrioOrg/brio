f3b34c8c63e3db1ae9ce8e5291b6e8f3b53c4583
Pierce Broudin <piercebroudin@macbookair.home>
2026-08-24 12:47:27 +0200

    feat(content): add figure types, buildDrawingModel, and geometry helpers (#37)

--- packages/content/src/index.ts
+++ packages/content/src/index.ts
@@ -1,2 +1,28 @@
 export type { RichTextToken } from './rich-text'
 export { parseRichText } from './rich-text'
+
+export type {
+  LabelPlacement,
+  FigurePoint,
+  FigureSegment,
+  FigurePolygon,
+  FigureCircle,
+  FigureAngleMark,
+  FigureLengthMark,
+  FigureLabel,
+  FigureNumberLine,
+  CoordinateSpace,
+  FigureSpec,
+  DrawingSegment,
+  DrawingPolygon,
+  DrawingCircle,
+  DrawingRightAngleSquare,
+  DrawingAngleArc,
+  DrawingLengthTick,
+  DrawingDot,
+  DrawingPointLabel,
+  DrawingFreeLabel,
+  DrawingNumberLine,
+  DrawingModel,
+} from './figure'
+export { buildDrawingModel, angleDegrees, euclideanLength, isCollinear } from './figure'
